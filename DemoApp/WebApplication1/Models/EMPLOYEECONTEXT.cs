dba0282c1bd62c91dbb1a014a1bc38af4df1d528
Dinesh.V <dineshviswes@gmail.com>
2020-12-23 13:05:11 +0530

    Initial Commit from Vs code

---
+++ DemoApp/WebApplication1/Models/EMPLOYEECONTEXT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class EMPLOYEECONTEXT :DbContext
+    {
+        public EMPLOYEECONTEXT(DbContextOptions<EMPLOYEECONTEXT> options) : base(options)
+        {
+
+        }
+        public DbSet<tblSkill> tblSkills { get; set; }
+        public DbSet<tblEmployee> tblEmployees { get; set; }
+
+        public DbSet<EmployeeViewModel> AllEmployees { get; set; }
+    }
+}
